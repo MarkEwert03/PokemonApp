f104eb806a0a49b8dc042aae2aeaad0fff9c3822
Mark Ewert <mewert03@students.cs.ubc.ca>
2023-03-27 15:26:33 -0700

    users can now change their gender in trainer panel and get a png to represent that

--- src/main/ui/gui/panels/TrainerPanel.java
+++ src/main/ui/gui/panels/TrainerPanel.java
@@ -1,11 +1,14 @@
 package ui.gui.panels;
 
+import model.Trainer;
+
 import javax.swing.*;
 import java.awt.*;
 
 // represents a JPanel for users to modify their trainer information and see a sprite of themselves
 public class TrainerPanel extends ColorPanel {
     private JPanel spritePanel;
+    private JLabel spriteImageLabel;
 
     private JPanel infoPanel;
 
@@ -15,6 +18,10 @@ public class TrainerPanel extends ColorPanel {
 
     private JPanel genderPanel;
     private JLabel genderLabel;
+    private ButtonGroup genderButtonGroup;
+    private JRadioButton maleRadioButton;
+    private JRadioButton femaleRadioButton;
+    private JRadioButton otherRadioButton;
 
     private JButton confirmButton;
 
@@ -34,8 +41,9 @@ public class TrainerPanel extends ColorPanel {
         spritePanel = new JPanel();
         this.add(spritePanel);
         spritePanel.setBackground(colour);
-        ImageIcon maleTrainer = new ImageIcon("./data/sprites/male trainer.png");
-        spritePanel.add(new JLabel(maleTrainer));
+        ImageIcon img = new ImageIcon("./data/sprites/male trainer.png");
+        spriteImageLabel = new JLabel(img);
+        spritePanel.add(spriteImageLabel);
         spritePanel.setVisible(true);
 
         infoPanel = new JPanel();
@@ -57,27 +65,53 @@ public class TrainerPanel extends ColorPanel {
         nameTextField = new JTextField(10);
         namePanel.add(nameTextField);
 
-        genderPanel = new JPanel();
-        infoPanel.add(genderPanel);
-        genderPanel.setBackground(colour);
-        genderLabel = new JLabel("Trainer Gender:");
-        genderPanel.add(genderLabel);
+        setupGenderPanel();
 
         confirmButton = new JButton("Confirm Changes");
         infoPanel.add(confirmButton);
         confirmButton.addActionListener(e -> {
-            handleConfirmTrainerName();
+            handleConfirm();
         });
 
         descriptionLabel = new JLabel(newLineStringToMultilineLabel(myTrainer.toString()));
         infoPanel.add(descriptionLabel);
     }
 
+    // EFFECTS: setups up gender label and radio buttons in button group
+    private void setupGenderPanel() {
+        genderPanel = new JPanel();
+        infoPanel.add(genderPanel);
+        genderPanel.setBackground(colour);
+        genderLabel = new JLabel("Trainer Gender:");
+        genderPanel.add(genderLabel);
+
+        genderButtonGroup = new ButtonGroup();
+        maleRadioButton = new JRadioButton("Male");
+        genderButtonGroup.add(maleRadioButton);
+        genderPanel.add(maleRadioButton);
+        maleRadioButton.setSelected(true);
+
+        femaleRadioButton = new JRadioButton("Female");
+        genderButtonGroup.add(femaleRadioButton);
+        genderPanel.add(femaleRadioButton);
+
+        otherRadioButton = new JRadioButton("Other");
+        genderPanel.add(otherRadioButton);
+        genderButtonGroup.add(otherRadioButton);
+    }
+
     // EFFECTS: handles confirm button presses to the change trainer name button
-    private void handleConfirmTrainerName() {
+    private void handleConfirm() {
         String userTrainerName = nameTextField.getText();
         if (!userTrainerName.isBlank()) {
             myTrainer.setName(userTrainerName);
+            if (maleRadioButton.isSelected()) {
+                myTrainer.setGender(Trainer.Gender.MALE);
+            } else if (femaleRadioButton.isSelected()) {
+                myTrainer.setGender(Trainer.Gender.FEMALE);
+            } else {
+                myTrainer.setGender(Trainer.Gender.OTHER);
+            }
             updateLabels();
         } else {
             JOptionPane.showMessageDialog(null, "Invalid Trainer Name",
@@ -89,5 +123,12 @@ public class TrainerPanel extends ColorPanel {
     @Override
     public void updateLabels() {
         descriptionLabel.setText(newLineStringToMultilineLabel(myTrainer.toString()));
+        if (myTrainer.getGender().equals(Trainer.Gender.MALE)) {
+            spriteImageLabel.setIcon(new ImageIcon("./data/sprites/male trainer.png"));
+        } else if (myTrainer.getGender().equals(Trainer.Gender.FEMALE)) {
+            spriteImageLabel.setIcon(new ImageIcon("./data/sprites/female trainer.png"));
+        } else {
+            spriteImageLabel.setIcon(new ImageIcon("./data/sprites/other trainer.png"));
+        }
     }
 }
